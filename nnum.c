b214504f9be1790cb0929d50d44fdd53dc6010e0
hemahappy1925 <hemahappy1925@gmail.com>
2017-06-28 11:11:19 +0530

    Update nnum.c

--- nnum.c
+++ nnum.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-       int n,j,sum=0;
-       printf("Enter the Number : ");
-       scanf("%d",&n);
-       for(j=1;j<=n;j++)
-           {
-              printf("%d ",j);
-              sum=sum+j;
-           }
-              printf("\nSum of  %d is : %d ",n,sum);
-              return 0;
+ int n,i,sum=0;
+ printf("Enter the Number : ");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+  printf("%d ",i);
+ sum=sum+i;
+ }
+ printf("\nSum of  %d is : %d ",n,sum);
+return 0;
 }
