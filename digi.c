4ff0cd3ac9c12920da87bdf63d18cd744c178e55
hemahappy1925 <hemahappy1925@gmail.com>
2017-06-28 11:52:29 +0530

    Update and rename digit.c to digi.c

---
+++ digi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int base, exponent;
+    long long result = 1;
+    scanf("%d", &base);
+    scanf("%d", &exponent);
+    while (exponent != 0)
+    {
+        result *= base;
+        --exponent;
+    }
+    printf("Answer = %lld", result);
+    return 0;
+}
